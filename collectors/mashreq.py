75293b02721c08586325397e10de5b8e04c12f20
Risexai <aptechvds@gmail.com>
2026-07-29 18:16:32 +0530

    Create mashreq.py

---
+++ collectors/mashreq.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_jobs():
+    """
+    Returns a list of jobs.
+
+    Each job is a dictionary:
+    {
+        "company": "...",
+        "title": "...",
+        "location": "...",
+        "mode": "...",
+        "link": "...",
+        "date": "..."
+    }
+    """
+    jobs = []
+
+    # We will add the live Mashreq collector here
+    # after identifying the careers platform.
+
+    return jobs
