e469d7926005931146468350dca743bb165af1a8
Risexai <aptechvds@gmail.com>
2026-07-29 18:22:55 +0530

    Create config.py

---
+++ collectors/config.py
@@ -0,0 +1,18 @@
+KEYWORDS = [
+    "banking",
+    "operations",
+    "relationship manager",
+    "credit cards",
+    "retail banking",
+    "branch manager",
+    "service manager",
+    "customer service",
+    "trade operations",
+    "kYC",
+    "AML"
+]
+
+WORK_MODES = [
+    "Remote",
+    "Hybrid"
+]
