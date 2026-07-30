7f92e005761d551c2d047ce8dafd2c659f32d2a5
Risexai <aptechvds@gmail.com>
2026-07-30 11:05:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+# Telegram
+
+MAX_JOBS_PER_RUN = 20
+
+# Work Mode
+
+ALLOWED_MODES = [
+    "Remote",
+    "Hybrid"
+]
+
+# Keywords
+
+MINIMUM_MATCH_SCORE = 1
+
+# Files
+
+POSTED_FILE = "data/posted_jobs.json"
+CSV_FILE = "data/jobs.csv"
+LOG_FILE = "data/logs.json"
+
+# Sources
+
+ENABLE_REMOTEOK = True
+ENABLE_REMOTIVE = True
+ENABLE_GREENHOUSE = False
+ENABLE_WORKDAY = False
+ENABLE_ORACLE = False
+ENABLE_SUCCESSFACTORS = False
